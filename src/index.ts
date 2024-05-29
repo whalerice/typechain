15fcc07a8be188c326b0211879601879963cd261
taerimwoo <joy@5ivechain.com>
2024-05-29 14:07:55 +0900

    Chain

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,37 @@ class Block implements BlockShape {
 
   static calculateHash(prevHash: string, height: number, data: string) {
     const toHash = `${prevHash}${height}${data}`;
-    return toHash;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+
+  public addBlock(data: string) {
+    const block = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+    this.blocks.push(block);
+  }
+
+  public getBlocks() {
+    return [...this.blocks];
+    // return this.blocks;
+  }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth Block");
+
+console.log(blockchain.getBlocks());
